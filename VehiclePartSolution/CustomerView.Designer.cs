1f5f07c891de73d6341896a32ced783941266e5b
shehan1994 <anushka.shehan94@gmail.com>
2024-08-25 23:30:28 +0530

    Order details added to the customer view

--- VehiclePartSolution/CustomerView.Designer.cs
+++ VehiclePartSolution/CustomerView.Designer.cs
@@ -28,17 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.btnPlaceOrder = new System.Windows.Forms.Button();
             this.btnLogOut = new System.Windows.Forms.Button();
+            this.gridViewOrderHistory = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewOrderHistory)).BeginInit();
             this.SuspendLayout();
             // 
             // btnPlaceOrder
             // 
             this.btnPlaceOrder.BackColor = System.Drawing.Color.White;
             this.btnPlaceOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPlaceOrder.Location = new System.Drawing.Point(125, 124);
+            this.btnPlaceOrder.Location = new System.Drawing.Point(624, 542);
             this.btnPlaceOrder.Name = "btnPlaceOrder";
-            this.btnPlaceOrder.Size = new System.Drawing.Size(192, 34);
+            this.btnPlaceOrder.Size = new System.Drawing.Size(193, 40);
             this.btnPlaceOrder.TabIndex = 1;
             this.btnPlaceOrder.Text = "Place Order";
             this.btnPlaceOrder.UseVisualStyleBackColor = false;
@@ -48,31 +53,77 @@
             // 
             this.btnLogOut.BackColor = System.Drawing.Color.White;
             this.btnLogOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogOut.Location = new System.Drawing.Point(393, 12);
+            this.btnLogOut.Location = new System.Drawing.Point(745, 12);
             this.btnLogOut.Name = "btnLogOut";
-            this.btnLogOut.Size = new System.Drawing.Size(76, 34);
+            this.btnLogOut.Size = new System.Drawing.Size(72, 29);
             this.btnLogOut.TabIndex = 2;
             this.btnLogOut.Text = "Log out";
             this.btnLogOut.UseVisualStyleBackColor = false;
             this.btnLogOut.Click += new System.EventHandler(this.btnLogOut_Click);
+            // 
+            // gridViewOrderHistory
+            // 
+            this.gridViewOrderHistory.AllowUserToAddRows = false;
+            this.gridViewOrderHistory.AllowUserToDeleteRows = false;
+            this.gridViewOrderHistory.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gridViewOrderHistory.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.gridViewOrderHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridViewOrderHistory.Location = new System.Drawing.Point(12, 74);
+            this.gridViewOrderHistory.Name = "gridViewOrderHistory";
+            this.gridViewOrderHistory.ReadOnly = true;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gridViewOrderHistory.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.gridViewOrderHistory.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
+            this.gridViewOrderHistory.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridViewOrderHistory.Size = new System.Drawing.Size(805, 453);
+            this.gridViewOrderHistory.TabIndex = 54;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(315, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 31);
+            this.label1.TabIndex = 55;
+            this.label1.Text = "Customer Portal";
             // 
             // CustomerView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(481, 327);
+            this.ClientSize = new System.Drawing.Size(829, 594);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gridViewOrderHistory);
             this.Controls.Add(this.btnLogOut);
             this.Controls.Add(this.btnPlaceOrder);
             this.Name = "CustomerView";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Customer Portal";
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewOrderHistory)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button btnPlaceOrder;
         private System.Windows.Forms.Button btnLogOut;
+        private System.Windows.Forms.DataGridView gridViewOrderHistory;
+        private System.Windows.Forms.Label label1;
     }
 }
--- VehiclePartSolution/CustomerView.cs
+++ VehiclePartSolution/CustomerView.cs
@@ -1,20 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace VehiclePartSolution
 {
     public partial class CustomerView : Form
     {
+        private string connection_string;
         public CustomerView()
         {
             InitializeComponent();
+            connection_string = ConfigurationManager.ConnectionStrings["DB_Connection_String"].ConnectionString;
+            LoadOrderHistory();
+
+        }
+
+        private void LoadOrderHistory()
+        {
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            {
+                string query = @"SELECT u.[first_name], u.[last_name], o.[order_id],o.[order_status], o.[order_date], od.[item_type], od.[quantity], od.[price]," +
+                    " o.[total_price]  FROM [Orders] o " +
+                    "JOIN [Order_details] od ON o.[order_id] = od.[order_id] " +
+                    "JOIN [SYS_Users] u ON o.[customer_id] = u.[user_id] WHERE o.[customer_id] = "+LoggerSession.user_id;
+                Console.WriteLine("sql :"+query);
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable orderListTable = new DataTable();
+                adapter.Fill(orderListTable);
+                
+
+                if (orderListTable != null && orderListTable.Rows.Count > 0)
+                {
+                    gridViewOrderHistory.DataSource = orderListTable; 
+                    gridViewOrderHistory.Columns["first_name"].HeaderText = "First Name";
+                    gridViewOrderHistory.Columns["last_name"].HeaderText = "Last Name";
+                    gridViewOrderHistory.Columns["order_status"].HeaderText = "Order Status";
+                    gridViewOrderHistory.Columns["order_id"].HeaderText = "Order ID";
+                    gridViewOrderHistory.Columns["order_date"].HeaderText = "Order Date";
+                    gridViewOrderHistory.Columns["item_type"].HeaderText = "Item Type";
+                    gridViewOrderHistory.Columns["quantity"].HeaderText = "Quantity";
+                    gridViewOrderHistory.Columns["price"].HeaderText = "Price";
+                    gridViewOrderHistory.Columns["total_price"].HeaderText = "Total Amount";
+                }
+                else
+                {
+                    // Handle the case where there are no records
+                    MessageBox.Show("No order history found for this customer.");
+                }
+
+                
+                /*gridViewOrderHistory.Columns["model"].HeaderText = "Model";
+                gridViewOrderHistory.Columns["model"].Width = 200;
+                gridViewOrderHistory.Columns["price"].HeaderText = "Price";
+                gridViewOrderHistory.Columns["price"].Width = 150;
+                gridViewOrderHistory.Columns["year"].HeaderText = "Year";
+                gridViewOrderHistory.Columns["year"].Width = 150;
+                gridViewOrderHistory.Columns["stock"].HeaderText = "Stock";*/
+            }
         }
 
         private void btnPlaceOrder_Click_1(object sender, EventArgs e)
