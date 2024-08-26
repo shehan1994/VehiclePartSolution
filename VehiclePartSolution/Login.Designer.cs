62300151a692ab500920057ceda80d75c055972d
shehan1994 <anushka.shehan94@gmail.com>
2024-08-27 01:26:12 +0530

    Login view change

--- VehiclePartSolution/Login.Designer.cs
+++ VehiclePartSolution/Login.Designer.cs
@@ -35,15 +35,18 @@
             this.txt_password = new System.Windows.Forms.TextBox();
             this.btn_login = new System.Windows.Forms.Button();
             this.btn_signup = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(100, 40);
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(333, 63);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(264, 31);
+            this.label1.Size = new System.Drawing.Size(327, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "Login to Your Account";
             // 
@@ -51,7 +54,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(59, 102);
+            this.label2.Location = new System.Drawing.Point(297, 145);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(77, 19);
             this.label2.TabIndex = 1;
@@ -62,7 +65,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(60, 147);
+            this.label3.Location = new System.Drawing.Point(298, 190);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(76, 19);
             this.label3.TabIndex = 2;
@@ -72,18 +75,18 @@
             // txt_username
             // 
             this.txt_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_username.Location = new System.Drawing.Point(151, 98);
+            this.txt_username.Location = new System.Drawing.Point(389, 141);
             this.txt_username.Name = "txt_username";
-            this.txt_username.Size = new System.Drawing.Size(230, 26);
+            this.txt_username.Size = new System.Drawing.Size(271, 26);
             this.txt_username.TabIndex = 3;
             this.txt_username.TextChanged += new System.EventHandler(this.txt_username_TextChanged);
             // 
             // txt_password
             // 
             this.txt_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_password.Location = new System.Drawing.Point(151, 143);
+            this.txt_password.Location = new System.Drawing.Point(389, 186);
             this.txt_password.Name = "txt_password";
-            this.txt_password.Size = new System.Drawing.Size(230, 26);
+            this.txt_password.Size = new System.Drawing.Size(271, 26);
             this.txt_password.TabIndex = 4;
             this.txt_password.UseSystemPasswordChar = true;
             this.txt_password.TextChanged += new System.EventHandler(this.txt_password_TextChanged);
@@ -92,7 +95,7 @@
             // 
             this.btn_login.BackColor = System.Drawing.Color.White;
             this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_login.Location = new System.Drawing.Point(380, 197);
+            this.btn_login.Location = new System.Drawing.Point(581, 247);
             this.btn_login.Name = "btn_login";
             this.btn_login.Size = new System.Drawing.Size(79, 30);
             this.btn_login.TabIndex = 5;
@@ -105,7 +108,7 @@
             this.btn_signup.BackColor = System.Drawing.Color.White;
             this.btn_signup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_signup.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.btn_signup.Location = new System.Drawing.Point(206, 197);
+            this.btn_signup.Location = new System.Drawing.Point(389, 247);
             this.btn_signup.Name = "btn_signup";
             this.btn_signup.Size = new System.Drawing.Size(172, 30);
             this.btn_signup.TabIndex = 6;
@@ -113,12 +116,23 @@
             this.btn_signup.UseVisualStyleBackColor = false;
             this.btn_signup.Click += new System.EventHandler(this.btn_signup_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::VehiclePartSolution.Properties.Resources.login_car;
+            this.pictureBox1.Location = new System.Drawing.Point(27, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(227, 229);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(468, 238);
+            this.ClientSize = new System.Drawing.Size(690, 335);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btn_signup);
             this.Controls.Add(this.btn_login);
             this.Controls.Add(this.txt_password);
@@ -128,8 +142,9 @@
             this.Controls.Add(this.label1);
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Login Form";
             this.Load += new System.EventHandler(this.Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,6 +159,7 @@
         private System.Windows.Forms.TextBox txt_password;
         private System.Windows.Forms.Button btn_login;
         private System.Windows.Forms.Button btn_signup;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
