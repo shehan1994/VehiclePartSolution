a6b286c7775a95db6a40a0e33302c43dffd58e6d
shehan1994 <anushka.shehan94@gmail.com>
2024-08-28 00:16:47 +0530

    User mange view create

---
+++ VehiclePartSolution/UserManage.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace VehiclePartSolution
+{
+    partial class UserManage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GridViewUsers = new System.Windows.Forms.DataGridView();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.GridViewUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(369, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 31);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "User Management";
+            // 
+            // GridViewUsers
+            // 
+            this.GridViewUsers.AllowUserToAddRows = false;
+            this.GridViewUsers.AllowUserToDeleteRows = false;
+            this.GridViewUsers.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.GridViewUsers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.GridViewUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridViewUsers.Location = new System.Drawing.Point(12, 93);
+            this.GridViewUsers.Name = "GridViewUsers";
+            this.GridViewUsers.ReadOnly = true;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.GridViewUsers.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.GridViewUsers.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
+            this.GridViewUsers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.GridViewUsers.Size = new System.Drawing.Size(915, 425);
+            this.GridViewUsers.TabIndex = 21;
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.White;
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(857, 9);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(70, 27);
+            this.btnBack.TabIndex = 72;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(227, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 19);
+            this.label5.TabIndex = 74;
+            this.label5.Text = "Search Car :";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(318, 61);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(367, 26);
+            this.txtSearch.TabIndex = 73;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.White;
+            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.Location = new System.Drawing.Point(766, 537);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(161, 36);
+            this.btnRegister.TabIndex = 75;
+            this.btnRegister.Text = "Registration";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            // 
+            // UserManage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(939, 598);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.GridViewUsers);
+            this.Controls.Add(this.label1);
+            this.Name = "UserManage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "User Manage";
+            ((System.ComponentModel.ISupportInitialize)(this.GridViewUsers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView GridViewUsers;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnRegister;
+    }
+}
---
+++ VehiclePartSolution/UserManage.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VehiclePartSolution
+{
+    public partial class UserManage : Form
+    {
+        private string connection_string;
+        public UserManage()
+        {
+            InitializeComponent();
+            connection_string = ConfigurationManager.ConnectionStrings["DB_Connection_String"].ConnectionString;
+            LoadUsers();
+            this.txtSearch.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtSearch_KeyUp);
+        }
+
+        private void LoadUsers()
+        {
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            {
+                string query = "SELECT username, role, first_name, last_name, email, phone_number, address from SYS_Users";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable userListTable = new DataTable();
+                adapter.Fill(userListTable);
+
+
+                if (userListTable != null && userListTable.Rows.Count > 0)
+                {
+
+                    foreach (DataRow row in userListTable.Rows)
+                    {
+                        if (row["role"] != DBNull.Value)
+                        {
+                            int role = Convert.ToInt32(row["role"]);
+                            row["role"] = role == 1 ? "Admin" : (role == 2 ? "Customer" : "Unknown");
+                        }
+                    }
+
+                    GridViewUsers.DataSource = userListTable;
+                    GridViewUsers.Columns["username"].HeaderText = "User Name";
+                    GridViewUsers.Columns["username"].Width = 100;
+                    GridViewUsers.Columns["role"].HeaderText = "Role";
+                    GridViewUsers.Columns["first_name"].HeaderText = "First Name";
+                    GridViewUsers.Columns["first_name"].Width = 200;
+                    GridViewUsers.Columns["last_name"].HeaderText = "Last Name";
+                    GridViewUsers.Columns["last_name"].Width = 200;
+                    GridViewUsers.Columns["email"].HeaderText = "Email";
+                    GridViewUsers.Columns["phone_number"].HeaderText = "Phone No.";
+                    GridViewUsers.Columns["address"].HeaderText = "Address";
+                    GridViewUsers.Columns["address"].Width = 300;
+                }
+                else
+                {
+                    // Handle the case where there are no records
+                    MessageBox.Show("No order history found for this customer.");
+                }
+            }
+        }
+
+        private void txtSearch_KeyUp(object sender, KeyEventArgs e)
+        {
+            string filterText = txtSearch.Text.Trim();
+            DataTable carTable = (DataTable)GridViewUsers.DataSource;
+
+            if (carTable != null)
+            {
+                if (string.IsNullOrEmpty(filterText))
+                {
+                    carTable.DefaultView.RowFilter = string.Empty;
+                }
+                else
+                {
+                    string rowFilter = $"Convert(username, 'System.String') LIKE '%{filterText}%' " +
+                                  $"OR Convert(first_name, 'System.String') LIKE '%{filterText}%'" +
+                                  $"OR Convert(last_name, 'System.String') LIKE '%{filterText}%'" +
+                                  $"OR Convert(address, 'System.String') LIKE '%{filterText}%'" +
+                                  $"OR Convert(phone_number, 'System.String') LIKE '%{filterText}%'" +
+                                  $"OR Convert(role, 'System.String') LIKE '%{filterText}%'" +
+                                  $"OR Convert(email, 'System.String') LIKE '%{filterText}%'";
+                    carTable.DefaultView.RowFilter = rowFilter;
+                }
+            }
+        }
+
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            AdminView admin = new AdminView();
+            admin.Show();
+            this.Close();
+        }
+    }
+}
